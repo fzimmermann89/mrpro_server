422d41206b0e5524a37e046c7dd627e15f20f15d
Felix Zimmermann <fzimmermann89@gmail.com>
2025-01-27 03:01:08 +0100

    check schema

--- build.py
+++ build.py
@@ -5,17 +5,27 @@ import subprocess
 import requests
 import zipfile
 from pathlib import Path
+import jsonschema
+
 
 def main():
     config_file = Path("config.json")
-    config_data = json.loads(config_file.read_text())
+    config_data = json.load(config_file.open())
+
+    schema_file = Path("siemens_schema.json")
+    schema_data = json.load(schema_file.open())
+    if errors := list(jsonschema.Draft7Validator(schema_data).iter_errors(config_data)):
+        print(errors)
+        raise ValueError("Json is not valid:\n" + "\n".join(errors))
+
     vendor = config_data["general"]["vendor"]
     name_en = config_data["general"]["name"]["en"]
     version = config_data["general"]["version"]
 
     name = f"OpenRecon_{vendor}_{name_en}_V{version}"
     tag = f"openrecon_{vendor.lower()}_{name_en.lower()}:v{version}"
-    config_encoded = base64.b64encode(config_file.read_bytes()).decode("utf-8")
+
+    config_encoded = base64.b64encode(json.dumps(config_data, indent=2).encode("utf-8")).decode("utf-8")
 
     print("Fetching latest mrpro version...")
     latest_mrpro_version = requests.get("https://pypi.org/pypi/mrpro/json").json()["info"]["version"]
@@ -26,14 +36,24 @@ def main():
         docker_image_path = tmp_dir_path / f"{name}.tar"
 
         print("Building Docker image...")
-        subprocess.run([
-            "docker", "build", "mrpro_server",
-            "-f", "Dockerfile",
-            "-t", tag,
-            "--build-arg", f"CONFIG={config_encoded}",
-            "--build-arg", f"VERSION={version}",
-            "--build-arg", f"MRPRO_VERSION={latest_mrpro_version}"
-        ], check=True)
+        subprocess.run(
+            [
+                "docker",
+                "build",
+                "mrpro_server",
+                "-f",
+                "Dockerfile",
+                "-t",
+                tag,
+                "--build-arg",
+                f"CONFIG={config_encoded}",
+                "--build-arg",
+                f"VERSION={version}",
+                "--build-arg",
+                f"MRPRO_VERSION={latest_mrpro_version}",
+            ],
+            check=True,
+        )
 
         print("Saving Docker image...")
         subprocess.run(["docker", "save", tag, "-o", str(docker_image_path)], check=True)
@@ -44,6 +64,7 @@ def main():
             archive.write("info.pdf", f"{name}.pdf")
 
     print(f"Archive created: {name}.zip")
+
 
 if __name__ == "__main__":
     main()
