a6193c9738fc409b18013e318b8dcfac864fdb94
Felix Zimmermann <fzimmermann89@gmail.com>
2025-02-05 14:21:55 +0100

    RSS

--- mrpro_server/process.py
+++ mrpro_server/process.py
@@ -1,16 +1,19 @@
-"""Processing"""
+"""Processing."""
 
+import gc
 import logging
+import resource
 import tempfile
-from collections.abc import Sequence
+from pathlib import Path
 
+import constants
 import ismrmrd
 import mrpro
 import torch
 
 
 def process(
-    acquisitions: Sequence[ismrmrd.Acquisition],
+    acquisitions: list[ismrmrd.Acquisition],
     config: dict,
     metadata: str,
     images: list[ismrmrd.Image],
@@ -18,42 +21,54 @@ def process(
 ) -> list[ismrmrd.Image]:
     """Process ISMRMRD Acquisitions to ISMRMRD Images."""
 
+    reference = acquisitions[0]  # Only needed until we can build header from IData
     with tempfile.TemporaryDirectory() as tmpdir:
         # TODO: create KData directly from ISMRMRD Acquisitions
-        file = tmpdir + "/data.mrd"
+        file = Path(tmpdir) / "data.mrd"
         with ismrmrd.Dataset(file) as dset:
             dset.write_xml_header(metadata)
-            for acq in acquisitions:
-                dset.append_acquisition(acq)
-        logging.info("KData written to %s", file)
+            while acquisitions:
+                dset.append_acquisition(acquisitions.pop())
+        logging.info(f"KData written to {file!s}. Size:  {file.stat().st_size}")
+        gc.collect()
 
         ##################################################
 
         kdata = mrpro.data.KData.from_file(file, ktrajectory=mrpro.data.traj_calculators.KTrajectoryCartesian())
-        recon = mrpro.algorithms.reconstruction.DirectReconstruction(kdata)
+        recon = mrpro.algorithms.reconstruction.DirectReconstruction(kdata, csm=None)
         image = recon(kdata)
-        logging.info("Reconstruction done. Image info: %s", str(image))
+
+        logging.info("Reconstruction done")
+        logging.debug(f"Memory used by Python process: {resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024:.2f} MB")
 
         ##################################################
 
-        # TODO: creae ISMRMRD Image from IData
-        data = image.data.detach().to(torch.complex64).cpu().flatten(end_dim=-5).numpy()
+        # TODO: create ISMRMRD Image from IData
+        data = image.rss(keepdim=True).detach().cpu().flatten(end_dim=-5).to(torch.complex64)
+        scale = (data.shape[-3:].numel()) ** 0.5
+        data = data * scale  # ICE uses no scaling in backward
         fov = kdata.header.recon_fov.apply(mrpro.utils.unit_conversion.m_to_mm)
-        images = []
+
+        new_images = []
         for i, current_data in enumerate(data):
             ismrmrd_image = ismrmrd.Image.from_array(
-                current_data, acquisition=acquisitions[0], transpose=False, field_of_view=(fov.x, fov.y, fov.z)
+                current_data.numpy(), acquisition=reference, transpose=False, field_of_view=(fov.x, fov.y, fov.z)
             )
+            directions = kdata.header.acq_info.orientation.reshape(-1)[0].as_directions()
+            logging.debug(f"Orientations {directions!s}")
             meta = ismrmrd.Meta(
                 {
                     "DataRole": "Image",
                     "ImageRowDir": [f"{v:.18f}" for v in ismrmrd_image.getHead().read_dir],
                     "ImageColumnDir": [f"{v:.18f}" for v in ismrmrd_image.getHead().phase_dir],
-                    "Keep_image_geometry": 1,
+                    "Keep_image_geometry": 0,
+                    "ImageComments": config["comment"],
                 }
             )
+
             ismrmrd_image.attribute_string = meta.serialize()
-            ismrmrd_image.image_index = i + 1
-            images.append(ismrmrd_image)
-        logging.info("ISMRMRD Images created")
-        return images
+            ismrmrd_image.image_index = i
+            ismrmrd_image.image_type = constants.IMTYPE_COMPLEX
+            new_images.append(ismrmrd_image)
+
+        return new_images
